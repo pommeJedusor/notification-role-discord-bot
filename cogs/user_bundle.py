e7fc5c6e42409612a87354c411fd5befbf341050
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-08-12 10:27:50 +0200

    remove deprecated commands

--- cogs/user_bundle.py
+++ cogs/user_bundle.py
@@ -2,115 +2,13 @@ import discord
 from discord import Guild, app_commands
 from discord.ext import commands
 
-from model.Bundle import Bundle
 from model.UserBundle import UserBundle
 from model.Permission import Permission
-from model.UserSerie import UserSerie
 
 
 class CogUsersBundles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @app_commands.command(
-        name="ajouter_bundle_a_user",
-        description="ajoute un bundle à un user",
-    )
-    async def add_bundle_to_user(
-        self,
-        interaction: discord.Interaction,
-        bundle: discord.Role,
-        user: discord.Member,
-    ):
-        if (
-            not Permission.is_user_powerfull(interaction)
-            and not user.id == interaction.user.id
-        ):
-            return await interaction.response.send_message(
-                "vous n'avez pas les permissions requises pour effectuer cette action",
-                ephemeral=True,
-            )
-
-        assert type(interaction.guild) is Guild
-
-        if not Bundle.getByServerAndRoleId(interaction.guild.id, bundle.id):
-            return await interaction.response.send_message(
-                "le role que vous avez entrez ne correspond à aucun bundle",
-                ephemeral=True,
-            )
-
-        if not user.get_role(bundle.id):
-            await user.add_roles(bundle)
-
-        UserBundle.save(interaction.guild.id, user.id, bundle.id)
-        UserSerie.addBundleSeriesToUser(interaction.guild.id, bundle.id, user.id)
-
-        # add discord roles for series in bundle
-        users_series = UserSerie.getByUserAndBundle(
-            interaction.guild.id, user.id, bundle.id
-        )
-        series = []
-        for user_serie in users_series:
-            global_user_serie = UserSerie.getByUserAndSerie(
-                interaction.guild.id, user.id, user_serie.serie_role_id
-            )
-            if not global_user_serie or global_user_serie.has_role:
-                series.append(interaction.guild.get_role(user_serie.serie_role_id))
-        series = [serie for serie in series if serie]
-        await user.add_roles(*series)
-
-        await interaction.response.send_message(
-            f"le bundle {bundle.name} a été ajouté au user: {user.name}",
-        )
-
-    @app_commands.command(
-        name="retirer_bundle_du_user",
-        description="retire un bundle d'un user",
-    )
-    async def remove_bundle_from_user(
-        self,
-        interaction: discord.Interaction,
-        bundle: discord.Role,
-        user: discord.Member,
-    ):
-        if (
-            not Permission.is_user_powerfull(interaction)
-            and not user.id == interaction.user.id
-        ):
-            return await interaction.response.send_message(
-                "vous n'avez pas les permissions requises pour effectuer cette action",
-                ephemeral=True,
-            )
-
-        assert type(interaction.guild) is Guild
-
-        if not Bundle.getByServerAndRoleId(interaction.guild.id, bundle.id):
-            return await interaction.response.send_message(
-                "le role que vous avez entrez ne correspond à aucun bundle",
-                ephemeral=True,
-            )
-
-        if user.get_role(bundle.id):
-            await user.remove_roles(bundle)
-
-        UserBundle.delete(interaction.guild.id, user.id, bundle.id)
-        # check wether he should still have the series and remove the discord role if such
-        users_series = UserSerie.getByUserAndBundle(
-            interaction.guild.id, user.id, bundle.id
-        )
-        UserSerie.deleteByBundleAndUser(interaction.guild.id, bundle.id, user.id)
-        series = [
-            interaction.guild.get_role(user_serie.serie_role_id)
-            for user_serie in users_series
-            if not UserSerie.getByUserAndSerie(
-                interaction.guild.id, user.id, user_serie.serie_role_id
-            )
-        ]
-        series = [serie for serie in series if serie]
-        await user.remove_roles(*series)
-        await interaction.response.send_message(
-            f"le bundle `{bundle.name}` a bien été retiré du user {user.name}",
-        )
 
     @app_commands.command(
         name="voir_bundles_utilisateur",
--- cogs/user_serie.py
+++ cogs/user_serie.py
@@ -2,7 +2,6 @@ import discord
 from discord import Guild, app_commands
 from discord.ext import commands
 
-from model.Serie import Serie
 from model.UserSerie import UserSerie
 from model.Permission import Permission
 
@@ -10,116 +9,6 @@ from model.Permission import Permission
 class CogUsersSeries(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @app_commands.command(
-        name="ajouter_serie_a_user",
-        description="ajoute une série à un user",
-    )
-    async def add_serie_to_user(
-        self,
-        interaction: discord.Interaction,
-        serie: discord.Role,
-        user: discord.Member,
-    ):
-        if (
-            not Permission.is_user_powerfull(interaction)
-            and not user.id == interaction.user.id
-        ):
-            return await interaction.response.send_message(
-                "vous n'avez pas les permissions requises pour effectuer cette action",
-                ephemeral=True,
-            )
-
-        assert type(interaction.guild) is Guild
-
-        if not Serie.getByServerAndRoleId(interaction.guild.id, serie.id):
-            return await interaction.response.send_message(
-                "le role que vous avez entrez ne correspond à aucune série",
-                ephemeral=True,
-            )
-
-        if not user.get_role(serie.id):
-            await user.add_roles(serie)
-
-        UserSerie.delete(interaction.guild.id, user.id, serie.id, 0)
-        UserSerie.save(interaction.guild.id, user.id, serie.id, True, 0)
-        await interaction.response.send_message(
-            f"la série {serie.name} a été ajouté au user: {user.name}",
-        )
-
-    @app_commands.command(
-        name="retirer_serie_du_user",
-        description="retire une série d'un user",
-    )
-    async def remove_serie_from_user(
-        self,
-        interaction: discord.Interaction,
-        serie: discord.Role,
-        user: discord.Member,
-    ):
-        if (
-            not Permission.is_user_powerfull(interaction)
-            and not user.id == interaction.user.id
-        ):
-            return await interaction.response.send_message(
-                "vous n'avez pas les permissions requises pour effectuer cette action",
-                ephemeral=True,
-            )
-
-        assert type(interaction.guild) is Guild
-
-        if not Serie.getByServerAndRoleId(interaction.guild.id, serie.id):
-            return await interaction.response.send_message(
-                "le role que vous avez entrez ne correspond à aucune série",
-                ephemeral=True,
-            )
-
-        UserSerie.delete(interaction.guild.id, user.id, serie.id, 0)
-
-        if user.get_role(serie.id) and not UserSerie.getByUserAndSerie(
-            interaction.guild.id, user.id, serie.id
-        ):
-            await user.remove_roles(serie)
-
-        await interaction.response.send_message(
-            f"la série `{serie.name}` a bien été retiré du user {user.name}",
-        )
-
-    @app_commands.command(
-        name="desactiver_serie_pour_user",
-        description="empêche un utilisateur d'avoir une série même s'il a un bundle qui l'inclut",
-    )
-    async def disable_serie_for_user(
-        self,
-        interaction: discord.Interaction,
-        serie: discord.Role,
-        user: discord.Member,
-    ):
-        if (
-            not Permission.is_user_powerfull(interaction)
-            and not user.id == interaction.user.id
-        ):
-            return await interaction.response.send_message(
-                "vous n'avez pas les permissions requises pour effectuer cette action",
-                ephemeral=True,
-            )
-
-        assert type(interaction.guild) is Guild
-
-        if not Serie.getByServerAndRoleId(interaction.guild.id, serie.id):
-            return await interaction.response.send_message(
-                "le role que vous avez entrez ne correspond à aucune série",
-                ephemeral=True,
-            )
-
-        if user.get_role(serie.id):
-            await user.remove_roles(serie)
-
-        UserSerie.delete(interaction.guild.id, user.id, serie.id, 0)
-        UserSerie.save(interaction.guild.id, user.id, serie.id, False, 0)
-        await interaction.response.send_message(
-            f"la série `{serie.name}` a bien été désactivé pour l'utilisateur {user.name}",
-        )
 
     @app_commands.command(
         name="voir_series_utilisateur",
